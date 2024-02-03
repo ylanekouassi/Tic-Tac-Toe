7aaff03736fb4384c6ad8211e0528857acdc44bf
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-02-03 18:17:16 -0500

    x and o displaying, feature is working

--- src/Components/Tictactoe.jsx
+++ src/Components/Tictactoe.jsx
@@ -7,13 +7,24 @@ const Tictactoe = () => {
 const [data, setData] = useState(Array(9).fill(""));
 const [current, setCurrent]= useState("X");
 
+const Display = (index) => {
+
+    const Field = data;
+    Field[index] = current;
+    setData(Field);
+    if(current === "X"){
+        setCurrent("O")
+    } else {
+        setCurrent("X")
+    }
+}
 
   return (
     <div className='Container'>
     <h1>Tic tac toe</h1>
     <div className='Field'>
         <div className='Line1'>
-            <div className="Boxes"></div>
+            <div className="Boxes" onClick={() => Display(0)}>{data[0]}</div>
             <div className="Boxes"></div>
             <div className="Boxes"></div>
         </div>

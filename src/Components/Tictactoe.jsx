10b707209562034f9d3b9e2409012b48d85120fb
ylanekouassi <121358143+ylanekouassi@users.noreply.github.com>
2024-02-03 18:30:31 -0500

    game field js completed, button feature started

--- src/Components/Tictactoe.jsx
+++ src/Components/Tictactoe.jsx
@@ -25,18 +25,18 @@ const Display = (index) => {
     <div className='Field'>
         <div className='Line1'>
             <div className="Boxes" onClick={() => Display(0)}>{data[0]}</div>
-            <div className="Boxes"></div>
-            <div className="Boxes"></div>
+            <div className="Boxes" onClick={() => Display(1)}>{data[1]}</div>
+            <div className="Boxes" onClick={() => Display(2)}>{data[2]}</div>
         </div>
         <div className='Line2'>
-            <div className="Boxes"></div>
-            <div className="Boxes"></div>
-            <div className="Boxes"></div>
+            <div className="Boxes" onClick={() => Display(3)}>{data[3]}</div>
+            <div className="Boxes" onClick={() => Display(4)}>{data[4]}</div>
+            <div className="Boxes" onClick={() => Display(5)}>{data[5]}</div>
         </div>
         <div className='Line3'>
-            <div className="Boxes"></div>
-            <div className="Boxes"></div>
-            <div className="Boxes"></div>
+            <div className="Boxes" onClick={() => Display(6)}>{data[6]}</div>
+            <div className="Boxes" onClick={() => Display(7)}>{data[7]}</div>
+            <div className="Boxes" onClick={() => Display(8)}>{data[8]}</div>
         </div>
     </div>
     <button>Reset</button>
